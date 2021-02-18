822daacce2c0207fdb4a752581960fc8626b6627
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-02-18 11:18:37 +0100

    noMongo

--- progenomes/src/mongoConnect.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-def mongoConnect():
-    """
-    Connect to eggNOG database using pymongo
-    """
-    client = MongoClient('10.0.3.1', 27017, maxPoolSize=10)
-    db = client.freeze11
-    return client, db
