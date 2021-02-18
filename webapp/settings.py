f34bc827fa78da9aaac9d70a955575f151926889
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-02-18 11:51:23 +0100

    GeCoVizCSS

--- webapp/settings.py
+++ webapp/settings.py
@@ -21,7 +21,7 @@ with open(BASE_DIR + '/.env/secretKey.txt') as f:
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  '.gmgfam.compgenomics.org',
