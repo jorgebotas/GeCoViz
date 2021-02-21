69483221e0898c8d9586ae913b4519cbdba70262
Jorge Botas <jorgebotasmiret@protonmail.com>
2021-02-21 10:45:02 +0100

    search

--- gmgfam/urls.py
+++ gmgfam/urls.py
@@ -5,15 +5,6 @@ from . import api
 urlpatterns = [
     # GMGFAM
     path('input/', views.input, name='input'),
-    path('clustercontext/<cluster>/<cutoff>/',
-         views.cluster_context,
-         name='cluster_context'),
-    path('genecontext/<unigene>/',
-         views.unigene_context,
-         name='unigene_context'),
-    path('listcontext/<genelist>/',
-         views.list_context,
-         name='list_context'),
     path('api/getcontext/<datatype>/<query>/<cutoff>/',
          api.get_context,
          name='get_context'),
